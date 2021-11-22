b32c66e9fa4c8846dd737f750e54cb8be5973c37
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-22 09:06:13 +0200

    start/stop btn developed

--- src/components/stop-watch.js
+++ src/components/stop-watch.js
@@ -1,33 +1,71 @@
 import { interval, scan } from "rxjs";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { toStopWatch } from "../services";
 
 const Default = () => {
-  const [time, setTime] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
   const intervalRef = useRef(null);
 
-  useEffect(() => {
+  const subscribeToTimer = (initSeconds = seconds) => {
     intervalRef.current = interval(1000)
-      .pipe(scan((time) => time + 1, 0))
-      .subscribe(setTime);
+      .pipe(scan((time) => time + 1, initSeconds))
+      .subscribe(setSeconds);
+  };
 
-    return () => intervalRef.current.unsubscribe();
-  }, []);
+  const unSubscribeToTimer = () => {
+    intervalRef.current.unsubscribe();
+    intervalRef.current = null;
+  };
 
+  const onHandleStart = () => {
+    setIsStarted(true);
+    subscribeToTimer();
+  };
+
+  const onHandleStop = () => {
+    setIsStarted(false);
+    unSubscribeToTimer();
+  };
+
+  const onHandleReset = () => {
+    if (!intervalRef.current) return setSeconds(0);
+    unSubscribeToTimer();
+    setSeconds(0);
+    subscribeToTimer(0);
+  };
   return (
     <main className="stop-watch">
       <div className="container p-3">
+        <h1>Stopwatch</h1>
+        <hr />
         <div className="row justify-content-center">
-          <div className="col-10 col-sm-8 col-md-6 col-lg-4 text-center position-relative">
+          <div className="col-10 col-sm-8 col-md-4 col-lg-4 col-xl-3 text-center position-relative">
             <div className="sceleton" />
-            <div>{toStopWatch(time)}</div>
+            <div>{toStopWatch(seconds)}</div>
           </div>
         </div>
 
-        <div className='text-center mt-5'>
-          <button className="btn btn-success btn-lg m-3">Start</button>
+        <div className="text-center mt-5">
+          {isStarted ? (
+            <button
+              className="btn btn-danger btn-lg m-3"
+              onClick={onHandleStop}
+            >
+              Stop
+            </button>
+          ) : (
+            <button
+              className="btn btn-success btn-lg m-3"
+              onClick={onHandleStart}
+            >
+              Start
+            </button>
+          )}
           <button className="btn btn-warning btn-lg m-3">Wait</button>
-          <button className="btn btn-danger btn-lg m-3">Reset</button>
+          <button className="btn btn-info btn-lg m-3" onClick={onHandleReset}>
+            Reset
+          </button>
         </div>
       </div>
     </main>
